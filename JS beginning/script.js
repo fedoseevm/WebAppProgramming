d6f62b30ccd474ff94374f4aa23bf8003c824618
fedoseevm <maksim.fedoseev122@gmail.com>
2023-09-14 08:27:55 +0100

    dodanie nowych rzeczy

--- JS beginning/script.js
+++ JS beginning/script.js
@@ -3,3 +3,5 @@ function test()
     alert("Witaj");
     // cialo funkci
 }
+
+document.write("<h1>Tytuł strony</h1> <b>Tekst wyświetlany przez document.write()</b> <br> <img src='cat.jpg' width='500px'> <br>");
