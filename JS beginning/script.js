096c19e4549b2d0bc2ab46d8612a89a672909c86
fedoseevm <maksim.fedoseev122@gmail.com>
2023-09-27 11:56:20 +0100

    dodanie funkcji szyfrowania

--- JS beginning/script.js
+++ JS beginning/script.js
@@ -1,7 +1,33 @@
+//                                             ---FUNKCJE---
 function test()
 {
     alert("Witaj");
     // cialo funkci
 }
 
+function test2()
+{
+    let wynik = (number1 * number2)? number1 * number2: number1 * number2; // ????????????????????
+    let method = (isNaN(wynik))?
+    "Wprowadź tylko liczby":
+    "Iloczyn liczb " + number1 + " i " + number2 + " wynosi " + wynik;
+    alert(method);
+}
+
+//                                      --- MOZLIWOSC WYPISYWANIA TEKSTU ---
 document.write("<h1>Tytuł strony</h1> <b>Tekst wyświetlany przez document.write()</b> <br> <img src='cat.jpg' width='500px'> <br>");
+
+//                                          --- SZYFROWANIE 
+
+// let znak = prompt("Podaj znak: ","");
+// alert(znak.charCodeAt(0));
+
+let login = prompt("Podaj login:");
+let password = prompt("Podaj hasło:");
+let haslo = password.charCodeAt(0);
+if (login == "admin" && haslo == 56) {
+    document.write("<body bgcolor='#AAFFAA'><h1>zalogowany</h1></body>")
+}
+else {
+    document.write("<body bgcolor='#FF5555'><h1>Nieprawidłowe login lub hasło</h1></body>");
+}
