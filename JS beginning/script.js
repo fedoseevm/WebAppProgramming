11fd1295937c278c70c94a9851165555fcf35ba2
fedoseevm <maksim.fedoseev122@gmail.com>
2023-10-05 08:14:53 +0100

    dodanie kodu z switch

--- JS beginning/script.js
+++ JS beginning/script.js
@@ -7,6 +7,8 @@ function test()
 
 function test2()
 {
+    let number1 = +prompt("Podaj pierwszą liczbę:");
+    let number2 = +prompt("Podaj drugą liczbę:");
     let wynik = (number1 * number2)? number1 * number2: number1 * number2; // ????????????????????
     let method = (isNaN(wynik))?
     "Wprowadź tylko liczby":
@@ -17,7 +19,7 @@ function test2()
 //                                      --- MOZLIWOSC WYPISYWANIA TEKSTU ---
 document.write("<h1>Tytuł strony</h1> <b>Tekst wyświetlany przez document.write()</b> <br> <img src='cat.jpg' width='500px'> <br>");
 
-//                                          --- SZYFROWANIE 
+//                                          --- SZYFROWANIE ---
 
 // let znak = prompt("Podaj znak: ","");
 // alert(znak.charCodeAt(0));
@@ -30,4 +32,18 @@ if (login == "admin" && haslo == 56) {
 }
 else {
     document.write("<body bgcolor='#FF5555'><h1>Nieprawidłowe login lub hasło</h1></body>");
+}
+
+//                                          --- SWITCH ---
+//                  --- W zaleznosci od podanej wartosci wykonuje rozne polecenia ---
+switch(prompt("Podaj imię","")) {
+    case "Daniel":
+        alert ("Co zrobiłem?");
+        break;
+    case "Stanisław":
+        alert("Matematyka!");
+        break;
+    case "Roma":
+        alert("bomba");
+        break;
 }
