4c44403bb1392f34e0d72c5635f70b82ebb2db82
fedoseevm <maksim.fedoseev122@gmail.com>
2024-09-27 14:53:06 +0200

    Modify index.html, script.js

--- Grafika SVG/Formularz tworzenie figury/script.js
+++ Grafika SVG/Formularz tworzenie figury/script.js
@@ -19,11 +19,15 @@ function DrawFigure() {
 
     if (figure === "rect") {
         svg = `<svg width="${size}px" height="${size}px">
-            <rect x="0" y="0" width="${size}" height="${size}" fill="${color}" stroke="black"></rect>
+            <g id="fugura">
+                <rect x="0" y="0" width="${size}" height="${size}" fill="${color}" stroke="black"></rect>
+            </g>
         </svg>`;
     } else if (figure === "circle") {
         svg = `<svg width="${size}px" height="${size}px">
-            <circle cx="${size / 2}" cy="${size / 2}" r="${size / 2}" fill="${color}" stroke="black"></circle>
+            <g id="fugura">
+                <circle cx="${size / 2}" cy="${size / 2}" r="${size / 2}" fill="${color}" stroke="black"></circle>
+            </g>
         </svg>`;
     }
 
