9d071a803db107f9564f8ca4f82c9e2ffed9ca2f
Fedoseev Maksim <maksim.fedoseev2022@technikum19.edu.pl>
2024-09-29 17:34:40 +0200

    Modify script.js

--- Grafika SVG/Formularz tworzenie figury/script.js
+++ Grafika SVG/Formularz tworzenie figury/script.js
@@ -18,18 +18,18 @@ function DrawFigure() {
     let svg = '';
 
     if (figure === "rect") {
-        svg = `<svg width="${size}px" height="${size}px">
-            <g id="fugura">
-                <rect x="0" y="0" width="${size}" height="${size}" fill="${color}" stroke="black"></rect>
+        svg = `<svg width="${size * 2}px" height="${size * 2}px">
+            <g id="figura">
+                <rect x="${size / 2}" y="${size / 2}" width="${size}" height="${size}" fill="${color}" stroke="black"></rect>
             </g>
         </svg>`;
     } else if (figure === "circle") {
         svg = `<svg width="${size}px" height="${size}px">
-            <g id="fugura">
+            <g id="figura">
                 <circle cx="${size / 2}" cy="${size / 2}" r="${size / 2}" fill="${color}" stroke="black"></circle>
             </g>
         </svg>`;
     }
 
-    document.getElementById("SVG_Container").innerHTML = svg;
+    document.getElementById("SVG_Container").innerHTML = svg;   
 }
